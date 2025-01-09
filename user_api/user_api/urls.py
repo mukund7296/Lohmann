1a568ff683d2e2b8c6b3e4153b1ab8aa0ea24e3f
Mukund Biradar <mukund2015@outlook.com>
2025-01-09 09:52:23 +0100

    Add files via upload

--- user_api/user_api/urls.py
+++ user_api/user_api/urls.py
@@ -21,6 +21,9 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),  # API base URL
